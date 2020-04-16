56448538e4a89c8a7b8ac6be13655fa1d2cba03c
kah-ve <cam.abdurrahman@gmail.com>
2020-04-16 17:07:06 -0400

    Updates

---
+++ precache-manifest.29571317bac94aa3d4f9f244ea70318b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffa79f8b7043104a3aeaf046c3907644",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "23ac33284d7535f942cf",
+    "url": "/my-app/static/css/main.2410b665.chunk.css"
+  },
+  {
+    "revision": "fbbd35578947155ceef3",
+    "url": "/my-app/static/js/2.674b4582.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.674b4582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23ac33284d7535f942cf",
+    "url": "/my-app/static/js/main.bf16ec47.chunk.js"
+  },
+  {
+    "revision": "293656d52bf581d13418",
+    "url": "/my-app/static/js/runtime-main.9e7aca70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
